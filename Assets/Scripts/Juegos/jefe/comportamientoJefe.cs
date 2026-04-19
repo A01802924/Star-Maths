f278d3040c4ec58b2389130f54259c81bc80f430
A01802924 <a01802924@tec.mx>
2026-04-18 23:34:08 -0600

    juego jefe
    casi esta terminado juego jefe falta arreglar errores minimos

--- Assets/Scripts/Juegos/jefe/comportamientoJefe.cs
+++ Assets/Scripts/Juegos/jefe/comportamientoJefe.cs
@@ -1,13 +1,74 @@
+using System.Collections;
 using UnityEngine;
 
 public class comportamientoJefe : MonoBehaviour
 {
     private int vida = 10;
-    [SerializeField] private lanzarMeteoritos lanzaMeteoritos;
+
+    [SerializeField]
+    private lanzarMeteoritos lanzaMeteoritos;
+
+    [SerializeField]
+    private controladorOperacionesJefe controlador;
 
     void Start()
     {
-        InvokeRepeating("RoundLanzar", 0f, 1.5f);
+        StartCoroutine(RutinaRounds());
+    }
+
+    IEnumerator RutinaRounds()
+    {
+        while (true)
+        {
+            controlador.generarOperacion();
+            controlador.MostrarInput(false);
+
+            float tiempo = 0f;
+
+            while (tiempo < 10f)
+            {
+                lanzaMeteoritos.Lanzar();
+                yield return new WaitForSeconds(1.5f);
+
+                tiempo += 1.5f;
+            }
+
+            controlador.respondido = false;
+            controlador.MostrarInput(true);
+
+            tiempo = 0f;
+
+            while (tiempo < 7.5f && !controlador.respondido)
+            {
+                yield return null;
+                tiempo += Time.deltaTime;
+            }
+
+            controlador.MostrarInput(false);
+
+            if (controlador.respondido)
+            {
+                if (controlador.inputRes == controlador.resultadoCorrecto)
+                {
+                    vidaNave.instance.correctas++;
+                }
+                else
+                {
+                    vidaNave.instance.vidas--;
+                    MenuPausa.instance.ActualizarVidas();
+                }
+            }
+            else
+            {
+                vidaNave.instance.vidas--;
+                MenuPausa.instance.ActualizarVidas();
+            }
+
+            if (vidaNave.instance.vidas <= 0 || vida <= 0)
+            {
+                yield break;
+            }
+        }
     }
 
     public void Golpe()
@@ -16,17 +77,8 @@ public class comportamientoJefe : MonoBehaviour
 
         if (vida <= 0)
         {
+            StopAllCoroutines();
             vidaNave.instance.Ganar(10);
         }
-    }
-
-    private void RoundLanzar()
-    {
-        lanzaMeteoritos.Lanzar();
-    }
-
-    private void RoundPregunta()
-    {
-        
     }
 }
--- Assets/Scripts/Juegos/vidaNave.cs
+++ Assets/Scripts/Juegos/vidaNave.cs
@@ -39,6 +39,6 @@ public class vidaNave : MonoBehaviour
         MenuPausa.instance.HUD.style.display = DisplayStyle.None;
         MenuPausa.instance.infoHUD.style.display = DisplayStyle.None;
 
-        StartCoroutine(MostrarMenu.instance.MuestraMenu(30.5f, vidas, 3, x + (3 - vidas), 5, 3 - vidas));
+        StartCoroutine(MostrarMenu.instance.MuestraMenu(30.5f, vidas, 3, x + (3 - vidas), x, 3 - vidas));
     }
 }
